a64059cc3d238e7a4a17eb0ed60211658e705f8f
ChrisKnowsDev <chrisharley81@gmail.com>
2021-02-23 17:15:41 -0500

    styled about page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import { Link } from 'react-router-dom'
-import About from '../pages/about'
 
 const Navbar = () => {
   return (
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,33 @@
-import React from 'react'
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+import { Link } from 'react-router-dom'
 
 const About = () => {
-  return <div>about page</div>
+  return (
+    <section>
+      <h3>About</h3>
+      <p>
+        tapApp is an application that allows you to enter a zip-code and see
+        what breweries are in your area. Select a brewery and get detailed
+        information including the brewery address and link to their website.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero, repellat
+        eveniet? Eum modi dolore iure totam beatae quo non! Nesciunt, dicta
+        labore! Beatae aliquam aperiam reiciendis dolor earum perferendis
+        exercitationem.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut officia
+        placeat eveniet veritatis rem ab nam in magni dicta iste mollitia odit,
+        cumque dolorum voluptates quidem eos necessitatibus dignissimos nobis
+        aspernatur sequi natus. Deleniti cupiditate harum incidunt dolores quae
+        saepe commodi numquam doloremque, ipsum expedita.
+      </p>
+      <Link to="/">
+        <button>back home</button>
+      </Link>
+    </section>
+  )
 }
 
 export default About
