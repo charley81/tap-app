90549bbeece7b9228e84c1a28b0b93644cf695cb
ChrisKnowsDev <chrisharley81@gmail.com>
2021-02-23 16:53:42 -0500

    added color scheme, navbar ui

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,19 +1,58 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
+import { Link } from 'react-router-dom'
+import About from '../pages/about'
 
 const Navbar = () => {
   return (
-    <nav
+    <header
       css={css`
-        background: red;
+        padding: 1rem;
+        background: var(--colorDark);
+        width: 100%;
+
+        a {
+          color: var(--colorLight);
+        }
+
+        nav {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          max-width: var(--maxWidth);
+          margin: auto;
+        }
+
+        span {
+          color: var(--colorPrimary);
+          text-shadow: 2px 2px 2px rgba(255, 255, 255, 0.7);
+        }
+
+        ul {
+          display: flex;
+        }
+
+        li {
+          padding: 0 0.5rem;
+        }
       `}
     >
-      <h1>tapApp</h1>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-      </ul>
-    </nav>
+      <nav>
+        <h1>
+          <Link to="/">
+            tap<span>App</span>
+          </Link>
+        </h1>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
