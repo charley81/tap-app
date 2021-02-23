8be67e85f64f61a7402aba2f36e6edb4bf92b96a
ChrisKnowsDev <chrisharley81@gmail.com>
2021-02-23 18:37:43 -0500

    created and styled ui for error page

--- src/pages/error.js
+++ src/pages/error.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
-  return <div>error page</div>
+  return (
+    <section>
+      <h3>Something went wrong...</h3>
+      <Link to="/">
+        <button>back home</button>
+      </Link>
+    </section>
+  )
 }
 
 export default Error
